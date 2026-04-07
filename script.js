4da2bbcf1b4e7a241a401279b4a69745a0e661e6
Endrias Eshetu <endriaseshetu75@gmail.com>
2026-04-07 20:41:00 +0300

    Refactor style groups and enhance layout for alpaca generator UI

--- script.js
+++ script.js
@@ -1,89 +1,77 @@
-const backgrounds = [
-  "assets/backgrounds/blue50.png",
-  "assets/backgrounds/blue60.png",
-  "assets/backgrounds/blue70.png",
-  "assets/backgrounds/darkblue30.png",
-  "assets/backgrounds/darkblue50.png",
-  "assets/backgrounds/darkblue70.png",
-  "assets/backgrounds/green50.png",
-  "assets/backgrounds/green60.png",
-  "assets/backgrounds/green70.png",
-  "assets/backgrounds/grey40.png",
-  "assets/backgrounds/grey70.png",
-  "assets/backgrounds/grey80.png",
-  "assets/backgrounds/red50.png",
-  "assets/backgrounds/red60.png",
-  "assets/backgrounds/red70.png",
-  "assets/backgrounds/yellow50.png",
-  "assets/backgrounds/yellow60.png",
-  "assets/backgrounds/yellow70.png",
-];
-
-const neckStyles = [
-  "assets/neck/default.png",
-  "assets/neck/bend-forward.png",
-  "assets/neck/bend-backward.png",
-  "assets/neck/thick.png",
-];
-
-const hairStyles = [
-  "assets/hair/default.png",
-  "assets/hair/bang.png",
-  "assets/hair/curls.png",
-  "assets/hair/elegant.png",
-  "assets/hair/quiff.png",
-  "assets/hair/short.png",
-];
-
-const eyeStyles = [
-  "assets/eyes/default.png",
-  "assets/eyes/angry.png",
-  "assets/eyes/naughty.png",
-  "assets/eyes/panda.png",
-  "assets/eyes/smart.png",
-  "assets/eyes/star.png",
-];
-
-const earStyles = [
-  "assets/ears/default.png",
-  "assets/ears/tilt-backward.png",
-  "assets/ears/tilt-forward.png",
-];
-
-const mouthStyles = [
-  "assets/mouth/default.png",
-  "assets/mouth/astonished.png",
-  "assets/mouth/eating.png",
-  "assets/mouth/laugh.png",
-  "assets/mouth/tongue.png",
-];
-
-const legStyles = [
-  "assets/leg/default.png",
-  "assets/leg/bubble-tea.png",
-  "assets/leg/cookie.png",
-  "assets/leg/game-console.png",
-  "assets/leg/tilt-backward.png",
-  "assets/leg/tilt-forward.png",
-];
-
-const accessoryStyles = [
-  "assets/accessories/headphone.png",
-  "assets/accessories/earings.png",
-  "assets/accessories/flower.png",
-  "assets/accessories/glasses.png",
-];
+const BASE = "assets";
 
 const styleGroups = {
-  background: { paths: backgrounds, layerId: "background" },
-  neck: { paths: neckStyles, layerId: "neck" },
-  hair: { paths: hairStyles, layerId: "hair" },
-  eyes: { paths: eyeStyles, layerId: "eye" },
-  ears: { paths: earStyles, layerId: "ear" },
-  mouth: { paths: mouthStyles, layerId: "mouth" },
-  leg: { paths: legStyles, layerId: "leg" },
-  accessory: { paths: accessoryStyles, layerId: "accessory" },
+  background: {
+    folder: "backgrounds",
+    items: [
+      "blue50",
+      "blue60",
+      "blue70",
+      "darkblue30",
+      "darkblue50",
+      "darkblue70",
+      "green50",
+      "green60",
+      "green70",
+      "grey40",
+      "grey70",
+      "grey80",
+      "red50",
+      "red60",
+      "red70",
+      "yellow50",
+      "yellow60",
+      "yellow70",
+    ],
+    layerId: "background",
+  },
+  neck: {
+    folder: "neck",
+    items: ["default", "bend-forward", "bend-backward", "thick"],
+    layerId: "neck",
+  },
+  hair: {
+    folder: "hair",
+    items: ["default", "bang", "curls", "elegant", "quiff", "short"],
+    layerId: "hair",
+  },
+  eyes: {
+    folder: "eyes",
+    items: ["default", "angry", "naughty", "panda", "smart", "star"],
+    layerId: "eye",
+  },
+  ears: {
+    folder: "ears",
+    items: ["default", "tilt-backward", "tilt-forward"],
+    layerId: "ear",
+  },
+  mouth: {
+    folder: "mouth",
+    items: ["default", "astonished", "eating", "laugh", "tongue"],
+    layerId: "mouth",
+  },
+  leg: {
+    folder: "leg",
+    items: [
+      "default",
+      "bubble-tea",
+      "cookie",
+      "game-console",
+      "tilt-backward",
+      "tilt-forward",
+    ],
+    layerId: "leg",
+  },
+  accessory: {
+    folder: "accessories",
+    items: ["headphone", "earings", "flower", "glasses"],
+    layerId: "accessory",
+  },
 };
+
+function buildAssetPath(folder, item) {
+  return `${BASE}/${folder}/${item}.png`;
+}
 
 function setActiveButton(sectionElement, clickedButton) {
   const buttons = sectionElement.querySelectorAll("button");
@@ -116,7 +104,7 @@ function setLayerImage(layerId, path) {
   layerImage.setAttribute("src", path);
 }
 
-function renderStyleButtons(paths, onSelect) {
+function renderStyleButtons(group, onSelect) {
   const styleContainer = document.getElementById("style");
   if (!styleContainer) {
     return;
@@ -124,13 +112,13 @@ function renderStyleButtons(paths, onSelect) {
 
   styleContainer.innerHTML = "";
 
-  paths.forEach((path) => {
+  group.items.forEach((item) => {
     const button = document.createElement("button");
     button.type = "button";
-    const label = path.split("/").pop().replace(".png", "").replace(/-/g, " ");
+    const label = item.replace(/-/g, " ");
     button.textContent = label.charAt(0).toUpperCase() + label.slice(1);
     button.onclick = function () {
-      onSelect(path);
+      onSelect(buildAssetPath(group.folder, item));
     };
     styleContainer.appendChild(button);
   });
@@ -142,42 +130,22 @@ function showStyleGroup(groupName) {
     return;
   }
 
-  renderStyleButtons(group.paths, (path) => {
+  renderStyleButtons(group, (path) => {
     setLayerImage(group.layerId, path);
   });
 }
 
-function changeBackground() {
-  showStyleGroup("background");
+function registerStyleGroupHandlers() {
+  Object.keys(styleGroups).forEach((groupName) => {
+    const functionName =
+      "change" + groupName.charAt(0).toUpperCase() + groupName.slice(1);
+
+    window[functionName] = function () {
+      showStyleGroup(groupName);
+    };
+  });
 }
 
-function changeNeck() {
-  showStyleGroup("neck");
-}
-
-function changeHair() {
-  showStyleGroup("hair");
-}
-
-function changeEyes() {
-  showStyleGroup("eyes");
-}
-
-function changeEars() {
-  showStyleGroup("ears");
-}
-
-function changeMouth() {
-  showStyleGroup("mouth");
-}
-
-function changeLeg() {
-  showStyleGroup("leg");
-}
-
-function changeAccessory() {
-  showStyleGroup("accessory");
-}
-
+registerStyleGroupHandlers();
 registerSectionActiveState("#accessories");
 registerSectionActiveState("#style");
